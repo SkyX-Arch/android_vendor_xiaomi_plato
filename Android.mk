9dd08b0b581365d40d4f4156f3101abc90923362
SkyX-Arch <draver162@gmail.com>
2026-07-05 16:49:27 +0700

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),plato)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,511337a8985a5492123a9db8da129a03d24ad643)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,c3e6edba752762f1c2c2c38066289dfdbaffdb51)
+$(call add-radio-file-sha1-checked,radio/ccu.img,628ea29cc5318c5b0d2d0f5f7f2ed5af8e7f3ecb)
+$(call add-radio-file-sha1-checked,radio/dpm.img,948b27939c32759493711f37f11fa34c8bfa5a46)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,ca5ac2b4a3a6ae2e581c9cb6498efceb7896593e)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,6baf906f5c70000ca7b4c8736a1c33ba29a02429)
+$(call add-radio-file-sha1-checked,radio/gz.img,dc29d6d25e088117af2cc0f59298fe58642d0a2e)
+$(call add-radio-file-sha1-checked,radio/lk.img,61018add715a81064fe0e470f842690124937c13)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,1214751ec70b4df135001319d07dbaa6330eacd1)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,3c73678470868dc97db91ddb447032c44d8a15cf)
+$(call add-radio-file-sha1-checked,radio/md1img.img,f1024b961d248f5cd86828bf6e97f0db47be2851)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,fe50ba3e5ee6662774565e4d38aa650b233d0ad7)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,d2383c5a6aa9a2607984421ef233abf3f73943b1)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,544f0a12fff598ed1866b695398d3b193b3ffc58)
+$(call add-radio-file-sha1-checked,radio/scp.img,748f30e15967b97f1ae71830755e4e0ec33ce931)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,edc9ca18cdf7a362e64c7ec5ed6a2bdc55746d80)
+$(call add-radio-file-sha1-checked,radio/sspm.img,2ab05c0710451b6bf18d126cb378c558dbc16e7d)
+$(call add-radio-file-sha1-checked,radio/tee.img,13aefdb314e2a9f3a3874c15722ebc470079926c)
+$(call add-radio-file-sha1-checked,radio/vcp.img,8150d9eb609795ad7357de5ef14b3bf43e34b260)
+
+endif
